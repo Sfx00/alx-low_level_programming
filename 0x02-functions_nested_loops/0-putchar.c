7fcaea056009c5812f5b6aa54c34b8b0840885a9
Oussama <oussamaba348@gmail.com>
2023-09-12 15:55:16 -0700

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - The main function of the program.
+ *
+ * This function is the entry point of the program. It returns an integer
+ * value to indicate the program's exit status.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(n[i]);
+	}
+	return (0);
+}
