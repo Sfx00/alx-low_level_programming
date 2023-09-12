6b2d889b78af41912a8ffeaa955899dd3d53ca63
Oussama <oussamaba348@gmail.com>
2023-09-12 16:34:04 -0700

    hello

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#includ "main.h"
+
+void print_alphabet(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 26; i++)
+		putchar(alp[i]);
+
+	putchar('\n');
+}
+
+
+int main(void)
+{
+	void print_alphabet;
+
+	return 0;
+}
