c6feac1bbd4792803e0a09a68b946d1c5d3b1fa1
Oussama <oussamaba348@gmail.com>
2023-09-12 17:34:49 -0700

    hello

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,9 @@
 
 int print_sign(int n)
 {
-			_putchar('+');
+	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -22,12 +24,3 @@ int print_sign(int n)
 		return (-1);
 	}
 }
-
-
-
-
-
-
-
-
-
