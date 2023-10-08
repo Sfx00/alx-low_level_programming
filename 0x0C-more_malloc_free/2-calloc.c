7d182b079f159095b19f5f09e2ed52b0882fc1c4
Oussama <oussamaba348@gmail.com>
2023-10-08 16:53:12 -0700

    hello

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _calloc - allocates memory for an array.
+* @num_items: number of elements.
+* @item_size: size of bytes.
+*
+* Return: pointer to the allocated memory.
+* if num_items or item_size is 0, returns NULL.
+* if malloc fails, returns NULL.
+*/
+
+void *_calloc(unsigned int num_items, unsigned int item_size)
+{
+	char *p;
+
+	unsigned int a;
+
+	if (num_items == 0 || item_size == 0)
+		return (NULL);
+
+	p = malloc(num_items * item_size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (a = 0; a < (num_items * item_size); a++)
+		p[a] = 0;
+
+	return (p);
+}
+
