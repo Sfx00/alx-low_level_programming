be4e1c53dc4c60ffc7ec273c3871ddb209e3404a
Oussama <oussamaba348@gmail.com>
2023-10-10 13:50:13 -0700

    hello

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,9 +2,7 @@
 
 /**
  * main - prints the name of the file
- *
  * program that prints the name of the file it was compiled from,followed by a new line.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
