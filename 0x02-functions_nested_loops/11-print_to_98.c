d3537ba3024e675623a81b137b9c437d769f2be1
SFX <oussamaba348@gmail.com>
2024-05-17 13:09:02 +0100

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,42 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+
+void print_to_98(int n)
 {
-    print_to_98(0);
-    print_to_98(98);
-    print_to_98(111);
-    print_to_98(81);
-    print_to_98(-10);
-    return (0);
+	int i;
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d",i);
+			if (i != 98)
+			{
+				putchar(',');
+			    putchar(' ');
+			}	
+			
+		}
+		
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d",i);
+			if (i != 98)
+			{
+				putchar(',');
+			    putchar(' ');
+			}
+		}
+		
+	}
+	else
+	    printf("%d",n);
+
+	putchar('\n');             
 }
